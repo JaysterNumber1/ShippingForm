ea3f482f9d762ab9d05a2252d0d28b9271a5921f
Jarrett S <jaysternumber1@gmail.com>
2022-11-30 10:06:57 -0700

    Shipping Form Haha

---
+++ functions.js
@@ -0,0 +1,18 @@
+
+function unfocus(){
+   document.getElementById("something").blur();
+}
+
+function resetTest(){
+
+    alert("Reset Test!");
+}
+
+function stylisticChange(){
+
+    var textBox = document.getElementById("something");
+    textBox.style.backgroundColor = "red";
+
+}
+
+
